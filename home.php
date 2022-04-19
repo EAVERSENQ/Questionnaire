a4a5799576d90277feb6d2d953f71fcbeaa09b76
EAVERSENQ <102042420+EAVERSENQ@users.noreply.github.com>
2022-04-19 09:56:50 +0200

    Add files via upload

---
+++ home.php
@@ -0,0 +1,56 @@
+<?php 
+session_start();
+include "connection.php";
+if (isset($_SESSION['id'])) {
+$query = "SELECT * FROM questions";
+$run = mysqli_query($conn , $query) or die(mysqli_error($conn));
+$total = mysqli_num_rows($run);
+?>
+
+<html>
+	<head>
+		<title>Questionnaire - Préambule</title>
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+		<link rel="stylesheet" type="text/css" href="css/style1.css">
+	</head>
+	<body>
+		<header>
+			<div class="container">
+				<h1>Questionnaire</h1>
+			</div>
+		</header>
+		<main>
+			<div class="container">
+				<?php
+					$email = $_SESSION['email'];
+					$query = "SELECT nom FROM users WHERE email = '$email' ";
+					$select_players = mysqli_query($conn, $query) or die(mysqli_error($conn));
+					$row = mysqli_fetch_array($select_players);
+					echo $row['nom'];
+				?>
+				<h2>Bienvenue sur le questionnaire</h2>
+				<p>Ce questionnaire est porté sur des questions de logique.</p>
+				<ul>
+				    <li><strong>Nombre de question : </strong><?php echo $total; ?></li>
+				    <li><strong>Type : </strong>Choix multiple</li>
+				    <li><strong>Temps estimé pour chaque question : </strong><?php echo $total * 0.05 * 60; ?> secondes</li>
+				    <li>Vous obtenez <strong>1 point</strong> pour chaque réponse correcte</li>
+				</ul>
+				<a href="question.php?n=1" class="start">Lancer</a>
+				<a href="exit.php" class="add">Retour</a>
+			</div>
+		</main>
+		<footer>
+			<div class="container">
+				© 2022 ELLIOT AVERSENQ. TOUS DROITS RÉSERVÉS.
+			</div>
+		</footer>
+	</body>
+</html>
+
+<?php unset($_SESSION['score']); ?>
+<?php }
+else {
+	header("location: index.php");
+}
+?>
---
+++ players.php
@@ -0,0 +1,74 @@
+<?php session_start(); ?>
+<?php include "connection.php";
+if (isset($_SESSION['admin'])) {
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Administration - Classement</title>
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+		<link rel="stylesheet" type="text/css" href="css/style1.css">
+	</head>
+
+	<body>
+		<header>
+			<div class="container">
+				<h1>Administration</h1>
+				<a href="index.php" class="start">Questionnaire</a>
+				<a href="add.php" class="start">Ajoutez une question</a>
+				<a href="allquestions.php" class="start">Toutes les questions</a>
+				<a href="players.php" class="start">Classement</a>
+				<a href="exit.php" class="start">Déconnexion</a>
+				
+			</div>
+		</header>
+
+		
+	<h1>Classement</h1>
+	<table class="data-table">
+		<caption class="title">Retrouvez ci-dessous tous les participants.</caption>
+		<thead>
+			<tr>
+			<th>Id</th>
+			<th>Nom</th>
+			<th>Email</th>
+			<th>Date</th>
+			<th>Points</th>
+			</tr>
+		</thead>
+		<tbody>
+		<?php 
+            
+            $query = "SELECT * FROM users ORDER BY score DESC";
+            $select_players = mysqli_query($conn, $query) or die(mysqli_error($conn));
+            if (mysqli_num_rows($select_players) > 0 ) {
+            while ($row = mysqli_fetch_array($select_players)) {
+                $id = $row['id'];
+                $nom = $row['nom'];
+                $email = $row['email'];
+                $played_on = $row['played_on'];
+                $score = $row['score'];
+                echo "<tr>";
+                echo "<td>$id</td>";
+                echo "<td>$nom</td>";
+                echo "<td>$email</td>";
+                echo "<td>$played_on</td>";
+                echo "<td>$score</td>";
+              
+                echo "</tr>";
+             }
+         }
+        ?>
+	
+		</tbody>
+		
+	</table>
+</body>
+</html>
+
+<?php } 
+else {
+	header("location: admin.php");
+}
+?>
+
